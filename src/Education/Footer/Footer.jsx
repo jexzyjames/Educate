b52ad91d0ab5374b9cc3cd4743289c5658c0e62a
jexzyjames <jexzyjames@gmail.com>
2025-02-04 11:21:55 +0100

    changed year to updated UTC year and video pause when i click on webpage

--- src/Education/Footer/Footer.jsx
+++ src/Education/Footer/Footer.jsx
@@ -2,9 +2,12 @@ import React, {useState, useEffect} from 'react'
 import {Button, Card, CardActions, CardContent, CardMedia, Container, Grid, Stack, Typography} from '@mui/material'
 import './Footer.css'
 const Footer = () => {
+  const copyright = new Date()
+  const year = copyright.getUTCFullYear()
+
   return (
     <div className='footer containers'>
-      <p>2024 Educate, All rights reserved</p>
+      <p>{year} Educate, All rights reserved</p>
       <ul>
         <li>Terms of Service</li>
         <li>Privacy Policy </li>
--- src/Education/PlayVideo/PlayVideo.jsx
+++ src/Education/PlayVideo/PlayVideo.jsx
@@ -3,16 +3,20 @@ import './PlayVideo.css'
 import video from '../Video/A Short Cornell University Campus Tour _ 4K (1).mp4'
 const PlayVideo = ({setPlay, play}) => {
   const player = useRef(null)
+  const stop = useRef(null)
   const closePlayer = (e)=>{
     if(e.target === player.current){
       setPlay(false)
+      stop.current.pause()
+      
 
     }
 
   }
   return (
     <div ref={player} onClick={closePlayer} className={`vid ${play? '' : 'hide'}`}>
-      <video src={video} autoPlay muted controls ></video>
+      <video
+       ref={stop} src={video} autoPlay muted controls ></video>
     </div>
   )
 }
